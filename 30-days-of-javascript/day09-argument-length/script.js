1c5ea57dc5df5140e02d54270e53efa180cccf30
VeeV <vahab.afsharian94@gmail.com>
2025-05-22 19:41:02 +0300

    Add day 09, 10, 11

---
+++ 30-days-of-javascript/day09-argument-length/script.js
@@ -0,0 +1,14 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function (...args) {
+  let arr = [...args];
+  return arr.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
+
+console.log(argumentsLength(1, 2, 3));
