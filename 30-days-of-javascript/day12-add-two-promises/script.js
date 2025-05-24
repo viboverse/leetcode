7206912403b638881ba98a67a9597a581c2820e7
VeeV <vahab.afsharian94@gmail.com>
2025-05-24 19:52:32 +0300

    Add day 12

--- 30-days-of-javascript/day12-add-two-promises/script.js
+++ 30-days-of-javascript/day12-add-two-promises/script.js
@@ -15,3 +15,8 @@ var addTwoPromises = async function (promise1, promise2) {
  */
 
 addTwoPromises(Promise.resolve(2), Promise.resolve(2)).then(console.log); // 4
+
+// Another Approach
+// const [v1, v2] = await Promise.all([promise1, promise2])
+
+// return v1 + v2;
