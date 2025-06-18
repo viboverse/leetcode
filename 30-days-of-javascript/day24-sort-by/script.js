46e89aa058ee046130b0043db9709805f2837212
VeeV <vahab.afsharian94@gmail.com>
2025-06-18 21:28:54 +0300

    Add day 24 ( Hard to FOCUS ) The war...

---
+++ 30-days-of-javascript/day24-sort-by/script.js
@@ -0,0 +1,10 @@
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function (arr, fn) {
+  return arr.sort((a, b) => fn(a) - fn(b));
+};
+
+console.log(sortBy([5, 4, 1, 2, 3], (x) => x));
