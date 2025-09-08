e455e3934ed2e09e855ac345e83f85fb6c3adbf2
vibo <vahab.afsharian94@gmail.com>
2025-09-08 09:35:59 +0300

    Add script for LeetCode #1 - Two Sum

---
+++ random-problems/easy/1-two-sum/script.js
@@ -0,0 +1,13 @@
+function twoSum(nums, target) {
+    var res = [];
+    for (var i = 0; i < nums.length; i++) {
+        for (var j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                res.push(i);
+                res.push(j);
+            }
+        }
+    }
+    return res;
+}
+console.log(twoSum([3, 2, 4], 6));
